4eb0b6eb390740268f9a2fc7ebd9985756013850
wato787 <wato787@icloud.com>
2023-02-07 04:53:16 +0900

    2/7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Header } from './components/Header'
 
 export const App = () => {
   return (
-    <div>App</div>
+<>
+<Header/>
+</>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <header style={{height:'100px',backgroundColor:'green',textAlign:'center'}}>
+        <p style={{color:'#fff',fontSize:'60px',fontWeight:'bold',lineHeight:'100px'}}>ToDo初自作</p>
+      </header>
+    </>
+  );
+};
